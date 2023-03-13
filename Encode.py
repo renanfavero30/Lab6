4649098d01a15ba237c5a9b86f14ac465725a783
Evangeline-Liu <127452252+Evangeline-Liu@users.noreply.github.com>
2023-03-13 12:38:42 -0400

    Update Encode.py

--- Encode.py
+++ Encode.py
@@ -8,6 +8,7 @@ while option != 3:
                        "1. Encode\n"
                        "2. Decode \n"
                        "3. Quit \n"
+                       "\n" # Spacing for Menu
                        "Please enter an option: "))
     if option == 1:
         password = (input("Please enter your password to encode: "))
@@ -16,12 +17,18 @@ while option != 3:
             new_digit = str((int(digit) + 3) % 10)
             new_password += new_digit
         password_encoded = new_password
-        print(f"Your password has been encoded and stored !")
-
+        print(f"Your password has been encoded and stored !"
+                "\n") # Spacing for Menu
 
     if option == 2:
-        pass
-
-        print(f"The encoded password is {password_encoded}, and the original password is {password_decode}.")
-
+        # Evangeline Decode
+        password_decoded = ""
+        for digit_ in new_password:
+            old_digit = int(digit_) - 3
+            if old_digit < 0:
+                old_digit = 9
+            password_decoded = password_decoded + str(old_digit)
+        
+        print(f"The encoded password is {password_encoded}, and the original password is {password_decoded}."
+               "\n") # Spacing for Menu
 
